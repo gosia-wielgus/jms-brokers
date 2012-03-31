342b351029e678e1ea0bd21e10765e2e1e576d22
admchr <admchr@gmail.com>
2012-03-31 02:59:29 +0200

    added non-working jms proof of concept support to server

--- server/src/main/java/pl/edu/agh/iosr/brokers/App.java
+++ server/src/main/java/pl/edu/agh/iosr/brokers/App.java
@@ -1,13 +1,43 @@
 package pl.edu.agh.iosr.brokers;
 
+import java.util.Properties;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         System.out.println( "Hello World!" );
+        
+        Properties properties = new Properties();
+        //properties.load(this.getClass().getResourceAsStream("hello.properties"));
+        Context context = new InitialContext(properties);
+        
+        TopicConnectionFactory connectionFactory = (TopicConnectionFactory) context.lookup("qpidConnectionfactory");
+        Connection connection = connectionFactory.createConnection(); 
+        connection.start();
+
+        Session session=connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+        Destination destination = (Destination) context.lookup("topicExchange"); 
+
+        MessageProducer messageProducer = session.createProducer(destination);  
+        MessageConsumer messageConsumer = session.createConsumer(destination);  
+
+        TextMessage message = session.createTextMessage("Hello world!");
+        messageProducer.send(message);
+
+        message = (TextMessage)messageConsumer.receive();    
+        System.out.println(message.getText());
+
+        connection.close(); 
+        context.close();
     }
 }
