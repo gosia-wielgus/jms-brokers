ee4291853f4d39480bc1b4729c0be79bc8487e48
admchr <admchr@gmail.com>
2012-05-12 16:13:32 +0200

    CA-signed keys work

--- keyz/create.sh
+++ keyz/create.sh
@@ -3,7 +3,7 @@
 echo generating CA cert:
 openssl genrsa -des3 -passout pass:password -out jms-brokers-ca.key 1024
 openssl req -new -passin pass:password -key jms-brokers-ca.key -subj "/C=US/ST=example/L=example/CN=JMS Brokers Sample CA" -out jms-brokers-ca.csr
-openssl x509 -req -in jms-brokers-ca.csr -passin pass:password -signkey jms-brokers-ca.key -outform PEM -out jms-brokers-ca.crt
+openssl x509 -days 3650 -req -in jms-brokers-ca.csr -passin pass:password -signkey jms-brokers-ca.key -outform PEM -out jms-brokers-ca.crt
 
 for store in client server ; do
     echo generating key for $store:
@@ -14,10 +14,16 @@ for store in client server ; do
     echo genetating sign requests:
     keytool -certreq -alias jms-brokers-$store -keypass password -storepass password -keystore $store.ks -file jms-brokers-$store.csr
 
+    keytool -export -alias jms-brokers-$store -keypass password -storepass password -keystore $store.ks -file jms-brokers-$store-ss.crt
+    
     echo signing certs:
-    openssl x509 -req -passin pass:password -in jms-brokers-$store.csr -signkey jms-brokers-ca.key -out jms-brokers-$store.crt
-
+    openssl x509 -days 3650 -req -passin pass:password -in jms-brokers-$store.csr -CA jms-brokers-ca.crt -CAkey jms-brokers-ca.key -CAcreateserial -out jms-brokers-$store.crt
+    cat jms-brokers-ca.crt jms-brokers-$store.crt >crt.chain
     echo importing certs:
-    keytool -keyalg RSA -import -alias jms-brokers-ca -storepass password -keystore $store.ks -file jms-brokers-ca.crt -trustcacerts -noprompt
-    keytool -keyalg RSA -import -alias jms-brokers-$store-byca -storepass password -keystore $store.ks -file jms-brokers-$store.crt -trustcacerts -noprompt
+    keytool -import -alias jms-brokers-ca -storepass password -keystore $store.ks -file jms-brokers-ca.crt -noprompt
+    keytool -import -alias jms-brokers-$store -storepass password -keystore $store.ks -file jms-brokers-$store.crt -noprompt
 done
+
+
+#keytool -import -alias jms-brokers-server -keypass password -storepass password -keystore client.ks -file jms-brokers-server-ss.crt -noprompt
+#keytool -import -alias jms-brokers-client -keypass password -storepass password -keystore server.ks -file jms-brokers-client-ss.crt -noprompt
