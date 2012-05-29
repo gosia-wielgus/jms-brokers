b89be96899f89d3dc5fdb11f9514a11b5abe32ff
Malgorzata Wielgus <gosia.wielgus@gmail.com>
2012-05-29 23:14:00 +0200

    stupid maven task to stop server nd sample test

---
+++ client/src/test/java/pl/edu/agh/iosr/brokers/intgration/ITSanityTest.java
@@ -0,0 +1,18 @@
+package pl.edu.agh.iosr.brokers.intgration;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class ITSanityTest {
+
+	@Test
+	public void testLoginPageDisplayed(){
+		WebDriver driver = new HtmlUnitDriver();
+		driver.get("http://localhost:8080");
+		assertEquals("Log in", driver.getTitle());
+	}
+	
+}
